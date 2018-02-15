9f29ebc75025a871214eb5de6902eb425f3e39c7
Blake Doty <blaked609@gmail.com>
2018-02-14 17:50:20 -0700

    Initial commit

--- src/com/pluralsight/myapp/Main.java
+++ src/com/pluralsight/myapp/Main.java
@@ -16,7 +16,7 @@ import com.pluralsight.calcengine.PowerOf;
 public class Main {
 
     public static void main(String[] args) {
-    	System.out.println("Hi");
+
 //		useMathEquation();
 //		useCalculatorBase();
 //		useCalculateHelper();
